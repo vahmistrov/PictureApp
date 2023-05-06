e887716e335c5840c098c2669978ca7b077294d0
vahmistrov <vahmistrov@list.ru>
2023-05-06 14:38:11 +0300

    Initial Commit

---
+++ PictureApp/PictureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictureAppApp.swift
+//  PictureApp
+//
+//  Created by Vitaly on 06.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PictureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
